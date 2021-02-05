d446c9867d61d433c3e5eb6f0aa3f8a7377a9f30
CassiniSea <78352344+CassiniSea@users.noreply.github.com>
2021-02-05 21:24:59 +0200

    Add Index.js

--- Index.js
+++ Index.js
@@ -1,9 +1,12 @@
 function alphabetPosition(text) {
-	return text	.toLowerCase()
-		.split(/\W/)
-		.join('')
+	let result = '';
+	text  .toLowerCase()
 		.split('')
-		.map((letter) => (letter.charCodeAt(0) - 96))
-		.join(' ');
+		.map((letter) => (
+			letter.charCodeAt(0) >= 97 && letter.charCodeAt(0) <= 122 ?
+				result += letter.charCodeAt(0) - 96 + ' ' :
+				''
+		))
+	return result.trim();
 }
 console.log(alphabetPosition("The sunset sets at twelve o' clock."))
